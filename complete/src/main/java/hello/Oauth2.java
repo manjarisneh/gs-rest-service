c4b13567ec8ab0422b56ab4230b7248e93ef5e48
Vishwajeet Rana <vishwajeetrana@Vishwajeets-MacBook-Air.local>
2019-12-17 23:10:27 +0530

    Oauth2.0

--- complete/src/main/java/hello/Oauth2.java
+++ complete/src/main/java/hello/Oauth2.java
@@ -4,6 +4,11 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.apache.groovy.json.internal.Chr;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
@@ -20,14 +25,28 @@ import static io.restassured.RestAssured.given;
 public class Oauth2 {
 
     @Test
-    public void accesscode() {
+    public void accesscode() throws InterruptedException {
 
 
         System.setProperty("webdriver.chrome.driver","//Users//vishwajeetrana//Documents//webdriver");
+        WebDriver driver=new ChromeDriver() ;
+
+        driver.get("https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https://rahulshettyacademy.com/getCourse.php&state=verifyfjdss");
+        driver.findElement(By.cssSelector("[type='email']")).sendKeys("dfsf");
+        driver.findElement(By.cssSelector("[type='email']")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("[type='password']")).sendKeys("vxcvd");
+        driver.findElement(By.cssSelector("[type='password']")).sendKeys(Keys.ENTER);
+        Thread.sleep(5000);
+        String url=driver.getCurrentUrl();
+        System.out.println(url);
+        String partialcode=url.split("code=")[1];
+        String code=partialcode.split("&scope")[0];
+        System.out.println(code);
 
         //----------------------------------
 
-        String tokenResponse = given().queryParams("code", "").
+        String tokenResponse = given().queryParams("code", code).
                 queryParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com").
                 queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W").
                 queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php").
